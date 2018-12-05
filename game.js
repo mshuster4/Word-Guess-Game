26c4444071fa14b0114290569776ef4b3fa930e8
Emroze <emrozesd@users.noreply.github.com>
2018-12-05 12:37:25 -0800

    Initial coe

---
+++ game.js
@@ -0,0 +1,11 @@
+//"Press any key to get started!"
+// Listen to what letter user will type
+//Create list of 10 types of words the computer will randomly choose from
+//Display word with "_ _ _ _" equal to the amount of letters in each word
+//If user guesses letter correctly, their letter is display
+//If user guess incorrectly, letter is displayed  under "letters already guessed"
+//User can't guess same letter twice! 
+//Guesses left: --
+//Once Guesses == 0 or user guesses all word letters 
+//Wins ++ or Losses ++
+//The next word is displayed 
